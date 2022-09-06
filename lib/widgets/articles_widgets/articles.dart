c6925c82ae95677c0221935221a162e056972cb9
Almantas1112 <aalmantass1112@gmail.com>
2022-09-06 15:05:29 +0300

    Quick bug fixes to UI

--- lib/widgets/articles_widgets/articles.dart
+++ lib/widgets/articles_widgets/articles.dart
@@ -24,124 +24,127 @@ class _ArticlesScreenState extends State<ArticlesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 280,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: widget.image != null
-                      ? NetworkImage('${widget.image}')
-                      : const AssetImage('images/default.png') as ImageProvider,
-                  fit: BoxFit.cover),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 280,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: widget.image != null
+                        ? NetworkImage('${widget.image}')
+                        : const AssetImage('images/default.png')
+                            as ImageProvider,
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                alignment: Alignment.bottomLeft,
+                child: SizedBox(
+                  width: 400,
+                  child: Text(
+                    widget.title != null ? '${widget.title}' : 'No Title',
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ),
+              ),
             ),
-            child: Container(
-              alignment: Alignment.bottomLeft,
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Text(
+                'Article',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
               child: SizedBox(
                 width: 400,
                 child: Text(
-                  widget.title != null ? '${widget.title}' : 'No Title',
+                  widget.content != null ? '${widget.content}' : 'No Content',
                   style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: const Text(
-              'Article',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              width: 400,
-              child: Text(
-                widget.content != null ? '${widget.content}' : 'No Content',
-                style: const TextStyle(
-                  fontSize: 13,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: const Text(
-              'More Details',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              width: 400,
-              child: Text(
-                widget.author != null ? '${widget.author}' : 'Uknown Author',
-                style: const TextStyle(
-                  fontSize: 13,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              width: 400,
-              child: Text(
-                widget.publishedAt != null
-                    ? 'Published at: ${widget.publishedAt}'
-                    : 'Uknown publishing time!',
-                style: const TextStyle(
-                  fontSize: 13,
-                ),
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.bottomRight,
-            margin: const EdgeInsets.only(top: 200, right: 20),
-            child: FloatingActionButton(
-              child: const Icon(Icons.arrow_back),
-              onPressed: () {
-                // print('Pressd!');
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const App(),
+                    fontSize: 13,
                   ),
-                );
-              },
+                ),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Text(
+                'More Details',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                width: 400,
+                child: Text(
+                  widget.author != null ? '${widget.author}' : 'Uknown Author',
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                width: 400,
+                child: Text(
+                  widget.publishedAt != null
+                      ? 'Published at: ${widget.publishedAt}'
+                      : 'Uknown publishing time!',
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomRight,
+              margin: const EdgeInsets.only(top: 200, right: 20),
+              child: FloatingActionButton(
+                child: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  // print('Pressd!');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const App(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
